4eed134da6b5384b35b66a93ab45cb861898dcc9
Gilbert Kibet <kibetgilly354@gmail.com>
2020-11-26 07:35:43 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'DEMOAPP';
+  // tslint:disable-next-line: variable-name
+  log_email: string;
+  // tslint:disable-next-line: variable-name
+  log_password: string;
+  // tslint:disable-next-line: typedef
+  // tslint:disable-next-line: variable-name
+  reg_email: string;
+  // tslint:disable-next-line: variable-name
+  reg_password: string;
+  // tslint:disable-next-line: variable-name
+  reg_confirm_password: string;
+// tslint:disable-next-line: typedef
+  register(){
+
+  }
+  // tslint:disable-next-line: typedef
+  login(){
+    // tslint:disable-next-line: curly
+  }
+  }
+
